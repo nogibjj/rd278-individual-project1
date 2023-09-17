8eac50927202715be4707bf30a0584a1df8d24a2
Rafael Davila Bugarin <rafaeldavila.b@gmail.com>
2023-09-17 18:36:27 +0000

    adding script

---
+++ pythonproject/src/script.py
@@ -0,0 +1,12 @@
+from lib import descriptive_statistics, generating_plot
+
+
+data,results=descriptive_statistics("pythonproject/src/data/median-income-by-country-2023.csv")
+
+generating_plot(
+    data,
+    x_variable="gdpPerCapitaPPP",
+    y_variable="meanIncome",
+    size="pop2023",
+    title="GDP per Capita vs Avg income (size proportional Population)",
+)
